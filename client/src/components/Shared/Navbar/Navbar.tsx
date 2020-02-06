6a23d485f5600bd8e6f2832edabba2fed41854fd
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-06 13:09:59 -0500

    Setup social links

--- client/src/components/Shared/Navbar/Navbar.tsx
+++ client/src/components/Shared/Navbar/Navbar.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import './Navbar.scss';
 import { default as NavbarBootstrap } from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 export default function Navbar() {
     return (
-        <NavbarBootstrap variant="dark" expand="lg">
+        <NavbarBootstrap variant="dark" expand="md">
             <Container fluid>
                 <NavbarBootstrap.Brand href="/">
                     Jordan Barbosa
                 </NavbarBootstrap.Brand>
+                <NavbarBootstrap.Toggle aria-controls="responsive-navbar-nav" />
+                <NavbarBootstrap.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto social-links d-flex flex-row justify-content-center">
+                        <Nav.Link href="https://github.com/jordanjordanb1/">
+                            <i className="fab fa-github"></i>
+                        </Nav.Link>
+
+                        <Nav.Link href="https://www.linkedin.com/in/jordan-barbosa/">
+                            <i className="fab fa-linkedin"></i>
+                        </Nav.Link>
+
+                        <Nav.Link href="mailto: jordansbarbosa01@gmail.com?subject=Contact from your website">
+                            <i className="far fa-envelope"></i>
+                        </Nav.Link>
+                    </Nav>
+                </NavbarBootstrap.Collapse>
             </Container>
         </NavbarBootstrap>
     );
