d01ad2c2d2eec80baa2ec324a1143c0b0f693368
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-06 00:12:29 -0500

    Finished home page

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import './Home.scss';
 import Navbar from '../Shared/Navbar/Navbar';
-import Footer from '../Shared/Footer/Footer';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 export default function Home() {
     return (
         <>
             <Navbar />
-            <Container></Container>
-            <Footer />
+            <Jumbotron
+                fluid
+                className="home-jumbotron d-flex justify-content-center align-items-center"
+            >
+                <div className="item-container">
+                    <div className="hi-box text-left">
+                        <h1>Hi,</h1>
+                    </div>
+                    <div className="intro-box text-left">
+                        <h1>I'm Jordan, </h1>
+                        <h1>a web developer</h1>
+                    </div>
+                </div>
+            </Jumbotron>
         </>
     );
 }
