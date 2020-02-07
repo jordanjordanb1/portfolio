8470f93c5b89c1dffe6c6301a2bf4bb5005276a0
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-07 00:00:25 -0500

    Moved navbar

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import './Home.scss';
-import Navbar from '../Shared/Navbar/Navbar';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 export default function Home() {
     return (
-        <>
-            <Navbar />
-            <Jumbotron
-                fluid
-                className="home-jumbotron d-flex justify-content-center align-items-center"
-            >
-                <div className="item-container">
-                    <div className="hi-box text-left">
-                        <h1>Hi,</h1>
-                    </div>
-                    <div className="intro-box text-left">
-                        <h1>I'm Jordan, </h1>
-                        <h1>a web developer</h1>
-                    </div>
+        <Jumbotron
+            fluid
+            className="home-jumbotron d-flex justify-content-center align-items-center"
+        >
+            <div className="item-container">
+                <div className="hi-box text-left">
+                    <h1>Hi,</h1>
                 </div>
-            </Jumbotron>
-        </>
+                <div className="intro-box text-left">
+                    <h1>I'm Jordan, </h1>
+                    <h1>a web developer</h1>
+                </div>
+            </div>
+        </Jumbotron>
     );
 }
