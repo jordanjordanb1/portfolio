5ddc1adaafc796c457e965912a04a4d0986b484e
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-07 22:50:30 -0500

    Added a projectitem component

---
+++ client/src/components/Projects/ProjectItem/ProjectItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Project from '../../../interfaces/ProjectInterface';
+import Image from 'react-bootstrap/Image';
+import './ProjectItem.scss';
+import { Link } from 'react-router-dom';
+
+interface Props {
+    key: number;
+    project: Project;
+}
+
+export default function ProjectItem({ project }: Props) {
+    return (
+        <Link
+            to={{
+                pathname: `/projects/${project.id}`,
+                state: project,
+            }}
+            className="project-item"
+        >
+            <div className="project-item-overlay d-flex justify-content-center align-items-center">
+                <div className="project-item-overlay-circle d-flex justify-content-center align-items-center">
+                    <span className="mt-1">VIEW</span>
+                </div>
+            </div>
+            <Image src="https://picsum.photos/350/250" />
+        </Link>
+    );
+}
