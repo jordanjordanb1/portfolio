0ecb42a199aa05eeb8b4fc2b9ed08fa93759778a
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-09 22:44:24 -0500

    Finished contact page

---
+++ client/src/components/Contact/Contact.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Contact.scss';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ContactForm from './ContactForm';
+
+export default function Contact() {
+    return (
+        <Jumbotron fluid>
+            <Container fluid>
+                <Row>
+                    <Col
+                        xs="12"
+                        md={{ span: 8, offset: 2 }}
+                        lg={{ span: 4, offset: 4 }}
+                        className="contact mt-3 mt-md-0"
+                    >
+                        <Row>
+                            <Col className="text-center">
+                                <h1>Contact me</h1>
+                            </Col>
+                        </Row>
+
+                        <hr />
+
+                        <Row noGutters>
+                            <Col
+                                xs="12"
+                                md={{ span: 10, offset: 1 }}
+                                lg={{ span: 8, offset: 2 }}
+                            >
+                                <ContactForm />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </Jumbotron>
+    );
+}
