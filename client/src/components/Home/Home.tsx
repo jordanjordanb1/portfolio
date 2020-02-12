52c74c0b72a00979b876ad993c28bea723c921d5
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-12 18:08:04 -0500

    Added contact cta and a resume button to home page

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Home.scss';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { config } from '../../config';
 
 export default function Home() {
     return (
@@ -15,6 +18,26 @@ export default function Home() {
                 <div className="intro-box text-left">
                     <h1>I'm Jordan, </h1>
                     <h1>a web developer</h1>
+                    <Button
+                        as={Link}
+                        to={{
+                            pathname: '/contact',
+                            state: {
+                                animation: 'slide-up',
+                            },
+                        }}
+                        className="quick-links mt-2 mr-2 p-2"
+                    >
+                        CONTACT ME
+                    </Button>
+                    <Button
+                        // @ts-ignore
+                        target="_blank"
+                        href={`${config.url}/resume/resume.pdf`}
+                        className="quick-links mt-2 mr-2 p-2 d-sm-none"
+                    >
+                        RESUME
+                    </Button>
                 </div>
             </div>
         </Jumbotron>
