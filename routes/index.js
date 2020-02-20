37d26637c089bd35b6787b42e4daf0334b41e37b
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-19 23:42:36 -0500

    Added catch all route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express'),
   router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/*', function (req, res, next) {
   res.render('index.html'); // Loads react application
 });
 
