f54a505bb83d67db40eb7132d60ba4a3e7c664f5
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-02-24 23:14:00 -0500

    BUILD: Pushed to heroku

--- public/precache-manifest.05a86191925f2a8b7d79eaea4e435862.js
+++ public/precache-manifest.05a86191925f2a8b7d79eaea4e435862.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17208e9c1ee50a9e5fff4073a26a4364",
+    "revision": "319cf00e543953d8aaee79ea599bfc14",
     "url": "/index.html"
   },
   {
-    "revision": "06efae5694b0a7ff466b",
-    "url": "/static/css/main.7ecfb988.chunk.css"
+    "revision": "157888387c80e5eea636",
+    "url": "/static/css/main.6f7ec71f.chunk.css"
   },
   {
     "revision": "00b01620a9d2b713d0a1",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.59d58302.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "06efae5694b0a7ff466b",
-    "url": "/static/js/main.f743ed33.chunk.js"
+    "revision": "157888387c80e5eea636",
+    "url": "/static/js/main.4e706edf.chunk.js"
   },
   {
     "revision": "5d93c6b2d15332364552",
