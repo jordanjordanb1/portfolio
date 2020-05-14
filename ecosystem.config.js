ef049a948816d78bf44d838f3dac2bfdefa39ca8
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-05-14 18:52:09 -0400

    Configured pm2 a bit more

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   apps: [
     {
+      name: 'portfolio',
       script: './bin/www',
       instances: '4',
+      exec_mode: 'cluster',
+      ignore_watch: [”[\/\\]\./”, “node_modules”],
       env: {
         NODE_ENV: 'production',
       },
