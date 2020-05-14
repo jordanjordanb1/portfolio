f531a0abb69a0b976205e9bc5ea653b84f4661e0
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-05-14 12:39:23 -0400

    Added an isDev variable

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,8 @@
 export default {
-    getUrl: () => {
-        return 'production'.includes(process.env.NODE_ENV) ? 'https://jordanbarbosa.com' : 'http://local.jordanbarbosa:3001'
-    },
+  isDevelopment: process.env.NODE_ENV === 'development' && true,
+  getUrl: () => {
+    return 'production'.includes(process.env.NODE_ENV)
+      ? 'https://jordanbarbosa.com'
+      : 'http://localhost:3001';
+  },
 };
