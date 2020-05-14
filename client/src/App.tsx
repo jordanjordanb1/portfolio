fc63b86f43da6aa1653a747002de36a1627f21d4
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-05-14 12:39:08 -0400

    App now handles routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { ReactElement } from 'react';
 import './App.scss';
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/ConfigureStore';
-import Routing from './components/Routing';
-
-const store = ConfigureStore();
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import SelectedProject from './components/SelectedProject/SelectedProject';
+import Contact from './components/Contact/Contact';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Shared/Navbar/Navbar';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import TrackedRoute from './hoc/analytics/Tracker';
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Provider store={store}>
-                <Routing />
-            </Provider>
-        </BrowserRouter>
-    );
+  return (
+    <>
+      <Navbar />
+      <Route
+        render={({ location }) => (
+          <TransitionGroup
+            component={null}
+            childFactory={(child): ReactElement<any> =>
+              React.cloneElement(child, {
+                classNames: location.state ? (location.state as any).animation : 'slide-down',
+              })
+            }
+          >
+            <CSSTransition key={location.pathname} timeout={750} mountOnEnter unmountOnExit>
+              <Switch location={location}>
+                <TrackedRoute exact path="/" component={Home} />
+                <TrackedRoute path="/about" component={About} />
+                <TrackedRoute exact path="/projects" component={Projects} />
+                <TrackedRoute path="/projects/:id" component={SelectedProject} />
+                <TrackedRoute path="/contact" component={Contact} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
+    </>
+  );
 }
