a8040831b36d22b3176d7502b0d5f034b74c3318
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-05-14 20:22:47 -0400

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'portfolio',
-      script: 'bin/www',
-      instances: '0',
+      script: './bin/www',
+      instances: '4',
       exec_mode: 'cluster',
       ignore_watch: ['noude_modules', 'client/node_modules'],
     },
